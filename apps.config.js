a8562faa519e68165faf63b8723d68455564b8b9
The Real VoidX3D <playzspreston2@gmail.com>
2025-10-05 11:45:04 +0545

    Update apps.config.js

--- apps.config.js
+++ apps.config.js
@@ -21,7 +21,7 @@ const apps = [
      {
         id: "chrome-dino",
         title: "Chrome-Dino",
-        icon: './themes/Yaru/status/dino-icon.gif',
+        icon: './themes/Yaru/status/dino-icon.png',
         disabled: false,
         favourite: true,
         desktop_shortcut: true,
